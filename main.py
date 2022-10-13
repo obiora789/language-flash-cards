1a860cab3afcf4ee534c13fc366a14a222b8dc63
Obiora <obioracelestine@gmail.com>
2022-10-13 10:32:38 +0100

    Push

--- main.py
+++ main.py
@@ -3,74 +3,66 @@ import pandas
 import random
 from tkinter import messagebox
 
-french_data = pandas.read_csv("./data/french_english.csv")
-spanish_data = pandas.read_csv("./data/spanish_english.csv")
-canvas_width = 800
-canvas_height = 526
+try:
+    french_data = pandas.read_csv("./data/french_words_to_learn.csv")
+    spanish_data = pandas.read_csv("/data/spanish_words_to_learn.csv")
+except FileNotFoundError:
+    french_data = pandas.read_csv("./data/french_english.csv")
+    spanish_data = pandas.read_csv("./data/spanish_english.csv")
+
+card_width = 800
+card_height = 526
 BACKGROUND_COLOR = "#B1DDC6"
+ENGLISH_COLOR = "white"
+LANGUAGE_COLOR = "black"
 PADDING = 40
 FONT_NAME = "Arial"
 english_word = ""
 language_word = ""
-count_timer = ""
-unique_ids = {"lang_id": 0, "word_id": 0, }
 language_index = 0
 front_language = ""
 english = "English"
+flip_to_english = False
 
 
-def switch_display(count):
-    global count_timer
-    if count > 0:
-        right_button.config(command="")
-        wrong_button.config(command="")
-        count_timer = window.after(1000, switch_display, count - 1)
-        front_card.grid()
-        back_card.grid_remove()
-    else:
-        window.after_cancel(count_timer)
-        back_card.grid()
-        front_card.grid_remove()
+def switch_to_english():
+    global flip_to_english
+    flip_to_english = True
+    card.itemconfig(front_id, image=back_img)
+    card.itemconfig(lang_id, text=english, fill=ENGLISH_COLOR)
+    card.itemconfig(word_id, text=english_word, fill=ENGLISH_COLOR)
+    if flip_to_english:
         right_button.config(command=delete_items)
         wrong_button.config(command=reset)
+    return flip_to_english
+
+
+def reset():
+    global flip_to_english
+    manipulate_list()
+    flip_to_english = False
+
+    card.itemconfig(front_id, image=front_img)
+    card.itemconfig(lang_id, text=front_language, fill=LANGUAGE_COLOR)
+    card.itemconfig(word_id, text=language_word, fill=LANGUAGE_COLOR)
+    window.after(3000, func=switch_to_english)
+    if not flip_to_english:
+        right_button.config(command="")
+        wrong_button.config(command="")
+    return flip_to_english
 
 
 def manipulate_list():
     global language_index, language_word, english_word, front_language
     choice_object = random.choice(language_dictionary)
-    front_language = [key for key, _ in choice_object.items() if _ == _]
+    front_language = [key for key, _ in choice_object.items() if key == "French" or key == "Spanish"]
     front_language = front_language[0]
     language_index = language_dictionary.index(choice_object)
     language_word = choice_object[front_language]
     english_word = choice_object[english]
+    write_data = pandas.DataFrame(language_dictionary)
+    write_data.to_csv(f"./data/{front_language.lower()}_words_to_learn.csv")
     return language_index, front_language, language_word, english_word
-
-
-def front_image(fronted_image):
-    front_card.create_image(canvas_width/2, canvas_height/2, image=fronted_image)
-    unique_ids["lang_id"] = front_card.create_text(canvas_width/2, canvas_height/3.5, fill="black",
-                                                   text=front_language, font=(FONT_NAME, 35, "italic"))
-    unique_ids["word_id"] = front_card.create_text(canvas_width/2, canvas_height/2, fill="black",
-                                                   text=language_word, font=(FONT_NAME, 50, "bold"))
-
-
-def back_image(backed_image):
-    back_card.create_image(canvas_width/2, canvas_height/2, image=backed_image)
-    unique_ids["lang_id"] = back_card.create_text(canvas_width/2, canvas_height/3.5, text=english,
-                                                  font=(FONT_NAME, 35, "italic"))
-    unique_ids["word_id"] = back_card.create_text(canvas_width/2, canvas_height/2, text=english_word,
-                                                  font=(FONT_NAME, 50, "bold"))
-
-
-def reset():
-    manipulate_list()
-    front_card.delete(unique_ids["lang_id"], unique_ids["word_id"])
-    back_card.delete(unique_ids["lang_id"], unique_ids["word_id"])
-    frontier_image = PhotoImage(file="./images/card_front.png")
-    backer_image = PhotoImage(file="./images/card_back.png")
-    front_image(frontier_image)
-    back_image(backer_image)
-    switch_display(3)
 
 
 def delete_items():
@@ -91,15 +83,13 @@ window.config(padx=PADDING, pady=PADDING, background=BACKGROUND_COLOR, highlight
 
 manipulate_list()
 
-front_card = Canvas(width=canvas_width, height=canvas_height, highlightthickness=0, bg=BACKGROUND_COLOR)
-front_img = PhotoImage(file="./images/card_front.png")
-front_image(front_img)
-front_card.grid(row=0, column=0, columnspan=2)
-
-back_card = Canvas(width=canvas_width, height=canvas_height, highlightthickness=0, bg=BACKGROUND_COLOR)
+card = Canvas(width=card_width, height=card_height, highlightthickness=0, bg=BACKGROUND_COLOR)
 back_img = PhotoImage(file="./images/card_back.png")
-back_image(back_img)
-back_card.grid(row=0, column=0, columnspan=2)
+front_img = PhotoImage(file="./images/card_front.png")
+front_id = card.create_image(card_width/2, card_height/2)
+lang_id = card.create_text(card_width/2, card_height/3.5, fill="black", font=(FONT_NAME, 35, "italic"))
+word_id = card.create_text(card_width/2, card_height/2, fill="black", font=(FONT_NAME, 50, "bold"))
+card.grid(row=0, column=0, columnspan=2)
 
 right_image = PhotoImage(file="./images/right.png")
 right_button = Button(image=right_image, highlightthickness=0, border=0)
@@ -108,6 +98,6 @@ right_button.grid(row=1, column=1)
 wrong_image = PhotoImage(file="./images/wrong.png")
 wrong_button = Button(image=wrong_image, highlightthickness=0, border=0)
 wrong_button.grid(row=1, column=0)
-switch_display(3)
+reset()
 
 window.mainloop()
